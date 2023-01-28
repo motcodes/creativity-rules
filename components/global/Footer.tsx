c9b00ea24d675628300e3bab7e84d8ad8bc8fc23
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2023-01-28 15:37:12 +0100

    feat: add footer header links

--- components/global/Footer.tsx
+++ components/global/Footer.tsx
@@ -1,14 +1,57 @@
-import { CustomPortableText } from 'components/shared/CustomPortableText'
-import { Block } from 'sanity'
+import { partition } from 'lib/partition'
+import Link from 'next/link'
+import { Image } from 'sanity'
+import { LinkItem } from 'types'
 
-export function Footer({ footer }: { footer: Block[] }) {
+export interface FooterProps {
+  logo: Image
+  links: Array<LinkItem>
+  socialLinks: Array<LinkItem>
+}
+
+const linkClass = `hover:underline`
+export function Footer({ links, logo, socialLinks }: FooterProps) {
+  const [linksLeft, linksRight] = partition(links, 2)
   return (
-    <footer className="bottom-0 w-full py-12 text-center md:py-20 border-t">
-      {footer && (
-        <CustomPortableText
-          paragraphClasses="text-md md:text-xl"
-          value={footer}
-        />
+    <footer className="bottom-0 w-full p-12 md:py-20 border-t flex flex-row justify-between">
+      <div className="flex flex-row gap-12">
+        {!!linksLeft?.length && (
+          <ul>
+            {linksLeft.map((item: LinkItem) => (
+              <li key={item.label}>
+                <Link href={item.slug} className="hover:underline">
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+        {!!linksRight?.length && (
+          <ul>
+            {linksRight.map((item: LinkItem) => (
+              <li key={item.label}>
+                <Link href={item.slug} className="hover:underline">
+                  {item.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      {!!socialLinks?.length && (
+        <ul>
+          {socialLinks.map((item) => (
+            <li key={item.label}>
+              <Link
+                href={item.slug}
+                className="hover:underline"
+                target="_blank"
+              >
+                {item.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
       )}
     </footer>
   )
